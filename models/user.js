61eaba3b4d85eceda2b86312a6899fd045ac1fa7
Andrew Wong <andrewsulliwong@gmail.com>
2017-06-02 19:17:47 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,10 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
-        id: {
-            autoIncrement: true,
-            primaryKey: true,
-            type: DataTypes.INTEGER
-        },
         firstname: {
             type: DataTypes.STRING,
             notEmpty: true
